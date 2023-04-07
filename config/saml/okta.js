be71257f74e43b74321752e58344ee66ca516342
Nick Benik <nbenik@gmail.com>
2023-04-07 11:36:04 -0400

    I2B2UI-433: Okta private key passwords now come from environment vars

--- config/saml/okta.js
+++ config/saml/okta.js
@@ -5,6 +5,11 @@ const IdentityProvider = saml.IdentityProvider;
 const fs = require('fs');
 const path = require('path');
 
+// handle passwords to access the private keys
+let keyPasswords = {sigPrivateKey: '', encPrivateKey: ''};
+if (process.env.OKTA_SIG_PRIVKEY_PASS) keyPasswords.sigPrivateKey = process.env.OKTA_SIG_PRIVKEY_PASS;
+if (process.env.OKTA_ENC_PRIVKEY_PASS) keyPasswords.encPrivateKey = process.env.OKTA_ENC_PRIVKEY_PASS;
+
 module.exports = {
     sp: (req) => {
         const samlURL = req.protocol + '://' + req.get('host') + '/saml/';
@@ -16,11 +21,11 @@ module.exports = {
             wantLogoutResponseSigned: true,
             wantLogoutRequestSigned: true,
             // the private key (.pem) use to sign the assertion;
-            privateKey: fs.readFileSync(path.join(__dirname, 'okta-encrypt.key')),
-            privateKeyPass: 'myprivatepassword',
+            privateKey: fs.readFileSync(path.join(__dirname, 'okta-sig.key')),
+            privateKeyPass: keyPasswords.sigPrivateKey,
             // the private key (.pem) use to encrypt the assertion;
             encPrivateKey: fs.readFileSync(path.join(__dirname, 'okta-encrypt.key')),
-            encPrivateKeyPass: 'myprivatepassword',
+            encPrivateKeyPass: keyPasswords.encPrivateKey,
             isAssertionEncrypted: true,
             assertionConsumerService: [{
                 Binding: saml.Constants.namespace.binding.post,
